9a8a929647266c9c2e7f9bc68185b26a8dd6d9bc
prettyboyswagondaline <g.araslanov2007@gmail.com>
2025-04-23 12:56:27 +0300

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,42 @@
+# # create list
+# a = []
+# b = list()
+# # retrive (read)
+# a.clear()
+# a.pop()
+# a.remove()
+a = [1, 2, 3]
+a.append(4)
+
+
+
+
+group_120 = ["агафанов","арасланов","дудин","дюндин","жариков","звягинцев","кириллова","колосков","кураев","кутовой","лесовой","мерзляков","невзоров"]
+group_1 = group_120[ :6]
+print(group_120)
+group_2 = group_120[6:]
+print(group_2)
+
+
+a = 1, 2, 3
+b = str(a)
+print(b)
+print(b))
+
+a = 1, 5.5, "hello"
+b = list(a)
+print(b)
+print(type(b))
+
+print(F "ЗАДАЧА 1")
+a = int(input("Введите четное число: - "))
+if a % 2 == 0 :
+    print(f"Число {a} является четным")
+else:
+    print(f"Число {a} является нечетным")
+
+print(f "задача 2")
+
+a = input("напишите свое предложение : - ")
+
+print(f"количество символов: - {len(a)}")
